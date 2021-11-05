cf686048adf795dab251e6e6729ad345e65e6f2a
EmilbMadsen <79310292+EmilbMadsen@users.noreply.github.com>
2021-11-05 10:56:50 +0100

    Added our model

---
+++ OurModel/Consumer/create_struct.m
@@ -0,0 +1,12 @@
+ 
+consumer_summer.time = [];
+consumer_summer.signals.values = [summer_weekday,summer_saturday,summer_sunday];
+consumer_summer.signals.dimensions =3;
+
+consumer_trans.time = [];
+consumer_trans.signals.values = [trans_weekday,trans_saturday,trans_sunday];
+consumer_trans.signals.dimensions =3;
+
+consumer_winter.time = [];
+consumer_winter.signals.values = [winter_weekday,winter_saturday,winter_sunday];
+consumer_winter.signals.dimensions =3;
---
+++ OurModel/load_global_variables.m
@@ -0,0 +1,7 @@
+number_households = 200000;
+number_windmills = 100;
+number_pv = 100;
+
+battery_capacity = 100; %kWh
+
+sim_time = 24;
