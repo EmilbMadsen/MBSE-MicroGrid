f8fca82b7d907ffa3a0c1b0b5d563b8b9b320dc1
CM134 <cm@christian-metz.de>
2021-11-11 15:14:51 +0100

    Controller implemented but Battery and Hydrogen are bad implemented.

--- OurModel/load_global_variables.m
+++ OurModel/load_global_variables.m
@@ -1,18 +1,28 @@
-number_households = 100;
-number_windmills = 1;
-number_pv = 100;
-
-battery_capacity = 100; %kWh
+number_households = 200000;
 
 sim_time = 24;
 
 %% Windturbine
 
+number_of_windturbines = 1;
 Rho = 1.204;
 Efficiency_wind = 0.4;
-length_of_wing = 44;
-A = length_of_wing^2*3.14;
+length_of_wing = 5;
+A = length_of_wing^2*3.14 * number_of_windturbines;
 
 %% Solar panel
 Efficiency_solar = 0.1;
-Area_panels = 10*number_households;%m2
+Area_panels = 700;%m2
+
+%% Hydrogen
+Max_H2_P_Input = 260000000;
+Min_H2_P_Input = 14000;
+number_fuel_cells = 12; 
+fuel_cell_power_rating = 5000;
+Start_Tank_Fill_W = 10000; % [W]
+
+%% Battery
+Battery_Capacity = 10000; %[W]
+minSOC = 5; %[%]
+maxSOC = 95; %[%]
+SOC_Start = 50; %[%] 
