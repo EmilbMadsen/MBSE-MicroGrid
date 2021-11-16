c1fe52bc13b0113dbe20d26c66d319aa6a6e2bea
CM134 <cm@christian-metz.de>
2021-11-16 18:20:16 +0100

    Changed Hydrogen

--- OurModel/load_global_variables.m
+++ OurModel/load_global_variables.m
@@ -22,8 +22,8 @@ Max_H2_P_Input = 260000000;
 Min_H2_P_Input = 1400;
 number_fuel_cells = 12; 
 
-fuel_cell_power_rating = 100e3;
-Start_Tank_Fill_L = 10e6; % [L]
+fuel_cell_power_rating = 50e6;
+Start_Tank_Fill_L = 1e3; % [L]
 
 P_electrolysis_max = 570000;
 P_electrolysis_min = 0;
@@ -36,9 +36,9 @@ H2_out_min = 0;
 Battery_Capacity = 100e3; %[Wh]
 minSOC = 60; % [%]
 maxSOC = 100; % [%]
-SOC_Start = 70; % [%] 
+SOC_Start = 100; % [%] 
 T_bat = 30*60; % timeconstant for loading
-number_cars = number_households/10;
+number_cars = 100%number_households/10;
 P_rating_bat_single = 7e3; % [W] output power wall
 
 P_charge_max = 150e3; %[W]
