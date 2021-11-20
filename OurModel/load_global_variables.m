72b547315aca3d03025e6b4c44f44f78259ebd5d
CM134 <cm@christian-metz.de>
2021-11-20 19:56:11 +0100

    controller update selfsupply

--- OurModel/load_global_variables.m
+++ OurModel/load_global_variables.m
@@ -23,7 +23,7 @@ Min_H2_P_Input = 1400;
 number_fuel_cells = 12; 
 
 fuel_cell_power_rating = 50e6;
-Start_Tank_Fill_L = 1e3; % [L]
+Start_Tank_Fill_L = 1000; % [L]
 
 P_electrolysis_max = 570000;
 P_electrolysis_min = 0;
@@ -38,7 +38,16 @@ minSOC = 60; % [%]
 maxSOC = 100; % [%]
 SOC_Start = 100; % [%] 
 T_bat = 30*60; % timeconstant for loading
-number_cars = 100%number_households/10;
+number_cars = number_households/3;
 P_rating_bat_single = 7e3; % [W] output power wall
 
 P_charge_max = 150e3; %[W]
+
+%% Controller
+
+H2_safety_buffer = 1e3;
+SOC_safety_buffer = 80;
+SOC_selfsupply = 98;
+H2_selfsupply = 10e3;
+controller_delay = 5;
+
